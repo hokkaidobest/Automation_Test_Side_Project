3dae2f2fc12db72302aaf3993ccc93607774d929
nmp <nmp@NMP.local>
2022-12-11 18:32:23 +0800

    [Feature] Completed product/category API part 1

--- side_project/sql_objects/product.py
+++ side_project/sql_objects/product.py
@@ -1,3 +1,7 @@
+import logging
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+
 from sql_objects.sql_utils import *
 
 class Product(SqlUtils):
@@ -36,3 +40,36 @@ class Product(SqlUtils):
         desc = self.cursor.description
         
         return {desc[0][0]: row[0], desc[1][0]: row[1]}
+
+    def get_product_by_category(self, category):
+        
+        if category == "all":
+            sql = "SELECT * FROM product;"
+        else:
+            sql = f"SELECT * FROM product WHERE category = '{category}';"
+        LOGGER.info(f"[DATA] SQL syntax is: {sql}")
+
+        self.cursor.execute(sql)
+        # Get selected data
+        rows = self.cursor.fetchall()
+        # Get column name
+        desc = self.cursor.description
+        
+        result = []
+        for row in rows:
+            result.append({
+                desc[0][0]: row[0], # id
+                desc[1][0]: row[1], # category
+                desc[2][0]: row[2], # title
+                desc[3][0]: row[3], # description
+                desc[4][0]: row[4], # price
+                desc[5][0]: row[5], # texture
+                desc[6][0]: row[6], # wash
+                desc[7][0]: row[7], # palce
+                desc[8][0]: row[8], # note
+                desc[9][0]: row[9], # story
+                desc[10][0]: row[10] # main_page
+            })
+        LOGGER.info(f"[DATA] SQL result: {result}")
+        
+        return result
---
+++ side_project/tests_api/test_api_product.py
@@ -0,0 +1,67 @@
+import logging, allure, pytest
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+
+from os import environ as env
+from dotenv import load_dotenv
+load_dotenv()
+
+from api_objects.product_category import ProductCategory
+from sql_objects.product import Product
+from test_data.get_data_from_excel import GetTestData
+
+base_url = env["UAT_URL"]
+
+get_data = GetTestData()
+success_login_data = get_data.get_success_login_data()
+failed_login_data = get_data.get_failed_login_data()
+failed_logout_data = get_data.get_failed_logout_data()
+
+# Test case 1：get product by category successfully
+# Test API: products/{category}?paging={paging}
+@pytest.mark.parametrize('category', ["men", "women", "accessories"])
+def test_get_product_by_category_successfully(session, category):
+
+    with allure.step("[START] test_get_product_by_category_successfully"):
+        paging = 0
+        api_result = []
+
+    while True:
+        
+        with allure.step("Send API request"):
+            url = base_url + f"api/1.0/products/{category}?paging={paging}"
+            product_api = ProductCategory(session)
+            response = product_api.get_product_by_category(url)
+
+        with allure.step("Verify API response status code"):
+            assert response.status_code == 200
+            LOGGER.info("[VERIFICATION] The http status code should be 200")
+
+        with allure.step("Save API response"):
+            api_result = api_result + response.json()["data"]
+
+        with allure.step("Check whether to send the request again"):
+            if not (response.json().get("next_paging") is None):
+                paging = response.json()["next_paging"]
+            else:
+                api_result_count = len(api_result)
+                break
+
+    with allure.step("Get product data via category"):
+        product_sql = Product()
+        db_result = product_sql.get_product_by_category(category)
+        db_result_count = len(db_result)
+
+    with allure.step("Verify API and DB same category count"):
+        assert api_result_count == db_result_count
+        LOGGER.info(f"[DATA] Count of API response: {api_result_count}, count of DB response: {db_result_count}")
+        LOGGER.info("[VERIFICATION] The product count of API and DB are same")
+
+    with allure.step("Check the category of each product"):
+        for product in api_result:
+            assert product["category"] == category
+            LOGGER.info(f"[VERIFICATION] The product: {product['title']} category is same to request")
+
+    with allure.step("[END] test_get_product_by_category_successfully"):
+        pass
