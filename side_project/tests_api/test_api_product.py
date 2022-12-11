2e0e7d13d086c8402f35bf8da8391dcd2d0cb926
nmp <nmp@NMP.local>
2022-12-11 18:37:13 +0800

    [Feature] Complete product/all API

--- side_project/tests_api/test_api_product.py
+++ side_project/tests_api/test_api_product.py
@@ -65,3 +65,46 @@ def test_get_product_by_category_successfully(session, category):
 
     with allure.step("[END] test_get_product_by_category_successfully"):
         pass
+
+# Test case 2：get all product successfully
+# Test API: products/{category}?paging={paging}
+@pytest.mark.parametrize('category', ["all"])
+def test_get_all_product_by_successfully(session, category):
+    
+    with allure.step("[START] test_get_all_product_by_successfully"):
+        paging = 0
+        api_result = []
+
+    while True:
+        
+        with allure.step("Send API request"):
+            url = base_url + f"api/1.0/products/{category}?paging={paging}"
+            product_api = ProductCategory(session)
+            response = product_api.get_product_by_category(url)
+
+        with allure.step("Verify API response status code"):
+            assert response.status_code == 200
+            LOGGER.info("[VERIFICATION] The http status code should be 200")
+
+        with allure.step("Save API response"):
+            api_result = api_result + response.json()["data"]
+
+        with allure.step("Check whether to send the request again"):
+            if not (response.json().get("next_paging") is None):
+                paging = response.json()["next_paging"]
+            else:
+                api_result_count = len(api_result)
+                break
+
+    with allure.step("Get product data via category"):
+        product_sql = Product()
+        db_result = product_sql.get_product_by_category(category)
+        db_result_count = len(db_result)
+
+    with allure.step("Verify API and DB same category count"):
+        assert api_result_count == db_result_count
+        LOGGER.info(f"[DATA] Count of API response: {api_result_count}, count of DB response: {db_result_count}")
+        LOGGER.info("[VERIFICATION] The product count of API and DB are same")
+
+    with allure.step("[END] test_get_all_product_by_successfully"):
+        pass
